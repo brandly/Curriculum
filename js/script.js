e2816ec146a7472afaefed2aa9190f91668c6430
Matthew Brandly <brandly13@gmail.com>
2012-03-24 22:57:45 -0400

    looks dope

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 
+function parse_form(inputs) {
+  var values = {};
+  inputs.each(function() {
+      values[this.name] = $(this).val();
+  });
+  return values;
+}
 
 
 (function (window, document, undefined) {
-
-  // Prepare window
-  $('#new_unit').tab('show');
 
   var Course = function() {
 
     this.units = [];
 
-    this.add_unit = function() {
+    this.add_unit = function(title) {
       var unit = {
-        name: "",
+        title: title,
         children: []
       };
 
@@ -20,48 +24,130 @@
       return unit;
     };
 
-    this.add_video = function(unit) {
+    this.add_video = function(unit, title, url) {
       var video = {
-        title: "",
-        url: ""
+        type: "video",
+        title: title,
+        url: url
       };
 
       unit.children.push(video);
       return video;
     };
 
-    this.add_question = function(unit) {
+    this.add_question = function(unit, question, answers, correct) {
       var question = {
-        question: "",
-        answer: ""
+        type: "question",
+        question: question,
+        answers: answers,
+        correct_answer: correct
       };
 
       unit.children.push(question);
       return question;
+    };
+
+    this.draw_course = function() {
+      var result = "<div id='class_display'><ul class='nav nav-list'>";
+      for(var u in this.units) {
+        var unit = this.units[u];
+        result += "<li><a href='#'><i class='icon-list-alt'></i>" + unit.title + "</a><ul class='nav nav-list'>";
+        for(var c in unit.children) {
+          var child = unit.children[c];
+          if(child.type === "question") {
+            result += "<li><a href='#'><i class='icon-question-sign'></i>" + child.question + "</a></li>";
+          } else if(child.type === "video") {
+            result += "<li><a href='#'><i class='icon-facetime-video'></i>" + child.title + "</a></li>";
+          }
+        }
+        result += "</ul></li>";
+      }
+      result += "</ul></div>";
+      // make way!
+      $('#class_display').remove();
+      // for the king?
+      $('#course_preview').append(result);
     };
   };
 
   // app
 
   window.course = new Course;
-  window.active = null;
+  course.active = null;
 
-  $('#unit_options').on('hide', function() {
-    var unit = course.add_unit();
-    active = unit;
-  })
+  // Editing Course
 
-  $( document ).on( 'click', '.new_unit', ( function( e ) {
-    $("#unit_options").modal();
+  $('#new_unit_form').submit(function() {
+    var inputs = $('#new_unit_form :input');
+    var values = parse_form(inputs);
+
+    course.active = course.add_unit(values.title);
+
+    // cleanup
+    $('#unit_title').attr('value', '');
+
+    course.draw_course();
+  });
+
+  $('#new_video_form').submit(function() {
+    if(course.active) {
+      var inputs = $('#new_video_form :input');
+      var values = parse_form(inputs);
+
+      course.add_video(course.active, values.title, values.url);
+
+      // cleanup
+      $('#video_title').attr('value', '');
+      $('#video_url').attr('value', '');
+
+      course.draw_course();
+    } else {
+      alert("No unit to add video to");
+    }
+  });
+
+
+  $('#new_question_form').submit(function() {
+    if(course.active) {
+      var inputs = $('#new_question_form :input');
+      var values = parse_form(inputs);
+      
+      var question = values.question;
+      var correct = $('input[name=correct_answer]:checked', '#new_question_form').val();
+      var answers = [];
+      
+      for(var i = 0; values["answer" + i]; i++) {
+        answers.push(values["answer" + i]);
+      }
+
+      course.add_question(course.active, question, answers, correct);
+
+      // cleanup
+      course.answer_count = 0;
+      $('.answer').remove();
+      $('#question_field').attr('value', '');
+
+      course.draw_course();
+    } else {
+      alert("No unit to add question to");
+    }
+});
+
+  // Editing Question
+  course.answer_count = 0;
+
+  $( document ).on( 'click', '#add_answer', ( function( e ) {
+    var count = course.answer_count;
+    $('<fieldset class="answer"><input type="text" class="span6" name="answer' + count + '" placeholder="Option ' + (count + 1) + '"><input class="span1" type="radio" name="correct_answer" value="' + count + '"></fieldset>').insertBefore(this);
+    course.answer_count += 1;
+    return false;
   }));
 
-  $( document ).on( 'click', '.new_video', ( function( e ) {
-    var video = course.add_video();
-  }));
-
-  $( document ).on( 'click', '.new_question', ( function( e ) {
-    var unit = course.add_unit();
-    active = unit;
+  $( document ).on( 'click', '#remove_answer', ( function( e ) {
+    $('.answer:last').remove();
+    if(course.answer_count > 0)
+      course.answer_count -= 1;
+    return false;
   }));
 
 })(this, document);
