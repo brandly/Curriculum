3edc7f297790abcf8cd7a42f7b38d24575c90e75
Matthew Brandly <brandly13@gmail.com>
2012-03-25 00:12:05 -0400

    minor reorganization

--- js/script.js
+++ js/script.js
@@ -51,13 +51,13 @@ function parse_form(inputs) {
       var result = "<div id='class_display'><ul class='nav nav-list'>";
       for(var u in this.units) {
         var unit = this.units[u];
-        result += "<li><a href='#'><i class='icon-list-alt'></i>" + unit.title + "</a><ul class='nav nav-list'>";
+        result += "<li><a href='#'><i class='icon-list-alt'></i> " + unit.title + "</a><ul class='nav nav-list'>";
         for(var c in unit.children) {
           var child = unit.children[c];
           if(child.type === "question") {
-            result += "<li><a href='#'><i class='icon-question-sign'></i>" + child.question + "</a></li>";
+            result += "<li><a href='#'><i class='icon-question-sign'></i> " + child.question + "</a></li>";
           } else if(child.type === "video") {
-            result += "<li><a href='#'><i class='icon-facetime-video'></i>" + child.title + "</a></li>";
+            result += "<li><a href='#'><i class='icon-facetime-video'></i> " + child.title + "</a></li>";
           }
         }
         result += "</ul></li>";
@@ -66,14 +66,23 @@ function parse_form(inputs) {
       // make way!
       $('#class_display').remove();
       // for the king?
-      $('#course_preview').append(result);
+      $('#course_layout').append(result);
+    };
+
+    this.JSON = function() {
+      return JSON.stringify(this);
     };
   };
 
   // app
+  var App = function() {
+    this.course = new Course;
+    this.active = null;
 
-  window.course = new Course;
-  course.active = null;
+
+  };
+
+  window.app = new App;
 
   // Editing Course
 
@@ -81,26 +90,26 @@ function parse_form(inputs) {
     var inputs = $('#new_unit_form :input');
     var values = parse_form(inputs);
 
-    course.active = course.add_unit(values.title);
+    app.active = app.course.add_unit(values.title);
 
     // cleanup
     $('#unit_title').attr('value', '');
 
-    course.draw_course();
+    app.course.draw_course();
   });
 
   $('#new_video_form').submit(function() {
-    if(course.active) {
+    if(app.active) {
       var inputs = $('#new_video_form :input');
       var values = parse_form(inputs);
 
-      course.add_video(course.active, values.title, values.url);
+      app.course.add_video(app.active, values.title, values.url);
 
       // cleanup
       $('#video_title').attr('value', '');
       $('#video_url').attr('value', '');
 
-      course.draw_course();
+      app.course.draw_course();
     } else {
       alert("No unit to add video to");
     }
@@ -108,7 +117,7 @@ function parse_form(inputs) {
 
 
   $('#new_question_form').submit(function() {
-    if(course.active) {
+    if(Curriculum.active) {
       var inputs = $('#new_question_form :input');
       var values = parse_form(inputs);
       
@@ -120,10 +129,10 @@ function parse_form(inputs) {
         answers.push(values["answer" + i]);
       }
 
-      course.add_question(course.active, question, answers, correct);
+      app.course.add_question(Curriculum.active, question, answers, correct);
 
       // cleanup
-      course.answer_count = 0;
+      app.answer_count = 0;
       $('.answer').remove();
       $('#question_field').attr('value', '');
 
@@ -134,19 +143,19 @@ function parse_form(inputs) {
 });
 
   // Editing Question
-  course.answer_count = 0;
+  app.answer_count = 0;
 
   $( document ).on( 'click', '#add_answer', ( function( e ) {
-    var count = course.answer_count;
+    var count = app.answer_count;
     $('<fieldset class="answer"><input type="text" class="span6" name="answer' + count + '" placeholder="Option ' + (count + 1) + '"><input class="span1" type="radio" name="correct_answer" value="' + count + '"></fieldset>').insertBefore(this);
-    course.answer_count += 1;
+    app.answer_count += 1;
     return false;
   }));
 
   $( document ).on( 'click', '#remove_answer', ( function( e ) {
     $('.answer:last').remove();
-    if(course.answer_count > 0)
-      course.answer_count -= 1;
+    if(app.answer_count > 0)
+      app.answer_count -= 1;
     return false;
   }));
 
